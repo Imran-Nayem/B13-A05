0ac35e5b8b722f6ff79fc9cb66e3c4d10a65df56
Imran Nayem <nayem.imran52@gmail.com>
2026-03-25 10:23:38 +0600

    html section

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if(user === "admin" && pass === "admin123") {
+    localStorage.setItem("isLoggedIn", "true"); 
+    window.location.href = "dashboard.html";   
+  } else {
+
+    document.getElementById("error").innerText = "Invalid login credentials";
+
+  }
+  
+}
